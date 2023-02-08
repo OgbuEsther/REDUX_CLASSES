b07795e1156ea8bd92583a0ae2c7cf3b7cf02815
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-08 12:27:45 -0800

    saved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/LoginPage';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<center style={{marginTop: "300px"}}>
+<LoginPage />
+   <Profile />
+</center>
+
     </div>
   );
 }
